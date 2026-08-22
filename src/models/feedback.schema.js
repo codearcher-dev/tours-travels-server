5566cc3fa795740b0117fdb7ff14b5d8584128a0
Rahul Vishwakarma <rahulrjp16@gmail.com>
2026-08-22 13:44:48 +0530

    Feedback schema created

---
+++ src/models/feedback.schema.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const feedbackSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    package: { type: mongoose.Schema.Types.ObjectId, ref: "Package" },
+    feedback: [{
+        question: { type: String },
+        rating: { type: Number, enum: [1, 2, 3, 4, 5], required: true }
+    }],
+    overallRating: { type: Number },
+    review: { type: String }
+}, { timestamps: true });
+
+const FeedbackModel = mongoose.model("Feedback", feedbackSchema);
+
+export default FeedbackModel;
