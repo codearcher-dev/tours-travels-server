b58af22e7a547fa1487fdf1c860d85750aa36a65
Rahul Vishwakarma <rahulrjp16@gmail.com>
2026-08-28 20:48:35 +0530

    cors origin updated

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(cookieParser());
 app.use(session({
     secret: process.env.SESSION_SECRET,
